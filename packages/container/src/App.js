a451492774b7c059867ec989feb9926b29d9008e
Kamal Yadav <kamalyadav@Kamals-MacBook-Pro.local>
2025-01-21 21:47:25 +0530

    updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import { MarketingApp } from './components/MarketingApp';
 export default () => {
 	return (
 		<>
-			<h1>Hello from the Container! </h1>
+			<h1>Hello from the Container!! </h1>
 			<MarketingApp />
 		</>
 	);
