514f8b83f1a98395ab329cb34c9a52e713cecf75
Kamal Yadav <kamalyadav@Kamals-MacBook-Pro.local>
2025-01-22 21:55:24 +0530

    added Header Component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { MarketingApp } from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
 	return (
 		<>
-			<h1>Hello from the Container!! </h1>
-			<MarketingApp />
+			<BrowserRouter>
+				<Header />
+				<MarketingApp />
+			</BrowserRouter>
 		</>
 	);
 };
