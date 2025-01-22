cc2d5e157b57e759eb1f905c723930301dfd3d23
Kamal Yadav <kamalyadav@Kamals-MacBook-Pro.local>
2025-01-22 22:33:24 +0530

    randomise css classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core';
+import { createGenerateClassName } from '@material-ui/core/styles';
+
 import { MarketingApp } from './components/MarketingApp';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'co',
+});
+
 export default () => {
 	return (
-		<>
-			<BrowserRouter>
+		<BrowserRouter>
+			<StylesProvider generateClassName={generateClassName}>
 				<Header />
 				<MarketingApp />
-			</BrowserRouter>
-		</>
+			</StylesProvider>
+		</BrowserRouter>
 	);
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { StylesProvider } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createGenerateClassName } from '@material-ui/core/styles';
 
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
 
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'ma',
+});
+
 export default () => {
 	return (
 		<>
-			<StylesProvider>
+			<StylesProvider generateClassName={generateClassName}>
 				<BrowserRouter>
 					<Switch>
 						<Route exact path="/pricing" component={Pricing} />
